0a00e9c9a8e6cb4fa4edd5d54e0175d8fb992e56
yaostyle <yaostyle@gmail.com>
2018-05-14 11:59:11 -0700

    Created Album class.

--- app/src/main/java/com/android/chrishsu/andplayer/Album.java
+++ app/src/main/java/com/android/chrishsu/andplayer/Album.java
@@ -1,24 +1,30 @@
 package com.android.chrishsu.andplayer;
 
+/* Album Class to store albums' information */
 public class Album {
+    // Init Vars
     private String mName;
     private String mArtist;
     private int mImageId;
 
+    // Album constructors to take 3 params: name, artist, & imageId
     public Album (String name, String artist,  int imageId) {
         this.mName = name;
         this.mArtist = artist;
         this.mImageId = imageId;
     }
 
+    // Gettter for name
     public String getName() {
         return this.mName;
     }
 
+    // Getter for artist
     public String getArtist() {
         return this.mArtist;
     }
 
+    // Getter for imageId
     public int getImageId() {
         return this.mImageId;
     }
